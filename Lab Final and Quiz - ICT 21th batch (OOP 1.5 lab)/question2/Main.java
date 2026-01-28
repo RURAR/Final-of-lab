552fff8d61d07d495cbca85d106bcfd6dd75cabb
RURAR <158255885+RURAR@users.noreply.github.com>
2026-01-28 15:18:54 +0600

    Create Main.java

---
+++ Lab Final and Quiz - ICT 21th batch (OOP 1.5 lab)/question2/Main.java
@@ -0,0 +1,29 @@
+class Employee {
+    double calculateSalary() {
+        return 0;
+    }
+}
+class FullTimeEmployee extends Employee {
+    @Override
+    double calculateSalary() {
+        return 140*200;
+    }
+}
+class PartTimeEmployee extends Employee {
+    @Override
+    double calculateSalary() {
+        return 80*200;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Employee emp;
+
+        emp=new FullTimeEmployee();
+        System.out.println("Full Time Salary: "+emp.calculateSalary());
+
+        emp=new PartTimeEmployee();
+        System.out.println("Part Time Salary: "+emp.calculateSalary());
+    }
+}
